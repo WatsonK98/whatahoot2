1cbd103c440c318e1339f057ad39ee7b51713784
aswatson <aswatson95@gmail.com>
2023-11-25 21:39:11 -0700

    added a game model

--- lib/pages/whatacaption/game_model.dart
+++ lib/pages/whatacaption/game_model.dart
@@ -1,1 +1,33 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class GameModel extends ChangeNotifier {
+  String _gameId = '';
+  String _hostNickname = '';
+  String _imageUrl = '';
+  List<String> _playerNicknames = [];
+
+  String get gameId => _gameId;
+  String get hostNickname => _hostNickname;
+  String get imageUrl => _imageUrl;
+  List<String> get playerNicknames => _playerNicknames;
+
+  void setGameId(String gameId) {
+    _gameId = gameId;
+    notifyListeners();
+  }
+
+  void setHostNickname(String hostNickname) {
+    _hostNickname = hostNickname;
+    notifyListeners();
+  }
+
+  void addPlayerNickname(String playerNickname) {
+    _playerNicknames.add(playerNickname);
+    notifyListeners();
+  }
+
+  void setImageUrl(String imageUrl) {
+    _imageUrl = imageUrl;
+    notifyListeners();
+  }
+}
