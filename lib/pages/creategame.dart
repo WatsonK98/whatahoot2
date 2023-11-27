1619df75ce53070d214db8d4a07f829b48bd05b7
aswatson <aswatson95@gmail.com>
2023-11-27 15:54:57 -0700

    fixed the qr code and random code generator

--- lib/pages/creategame.dart
+++ lib/pages/creategame.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whatahoot2/pages/whatacaption/createqr.dart';
 
 class CreateGamePage extends StatefulWidget {
@@ -9,7 +12,34 @@ class CreateGamePage extends StatefulWidget {
 }
 
 class _CreateGamePageState extends State<CreateGamePage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   final TextEditingController _textEditingController = TextEditingController();
+
+  //Random alphanumeric generator in lambda
+  final Random _rnd = Random();
+  final _chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+  ///Create join code method gets the user nickname and generated join code.
+  ///Saves the join code, and initializes the round to 0 to help with
+  ///indexing of online resources
+  Future<void> _createJoinCode() async {
+    final SharedPreferences prefs = await _prefs;
+    final String joinCode = getRandomString(5);
+
+    await prefs.setString('nickName', _textEditingController.text);
+    await prefs.setString('joinCode', joinCode);
+    await prefs.setInt('round', 0);
+  }
+
+  ///Dispose clears the controller and any other listeners except for
+  ///shared preferences
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
